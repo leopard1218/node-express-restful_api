0adf721999fd4d5a2a01a8b762feaec08b961f0a
leopard1218 <snowleopards1218@gmail.com>
2022-06-11 07:49:37 +0300

    fifth

--- db/database.js
+++ db/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-  db: 'https://cloud.mongodb.com/v2/62a405b83926791c335c730e#metrics/replicaSet/62a40643d477ac7559b21e5b/explorer'
+  db: 'mongodb+srv://snowleopards:hkc12188@cluster0.p8hsoma.mongodb.net/restapi?retryWrites=true&w=majority'
 };
