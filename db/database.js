d6c41d4ff5ae3899c201940b617c2a61a0967db0
leopard1218 <snowleopards1218@gmail.com>
2022-06-11 06:51:30 +0300

    third

--- db/database.js
+++ db/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-  db: 'https://cloud.mongodb.com/v2/62a405b83926791c335c730e#metrics/replicaSet/62a40643d477ac7559b21e5b/explorer'
+  db: 'https://cloud.mongodb.com/v2/62a405b83926791c335c730e#clusters'
 };
